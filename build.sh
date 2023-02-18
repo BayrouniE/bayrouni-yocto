7b086b30ee94540247c3cfb166a90b6bdbe28017
Ahmad Moutraji <ahmadmoutraji@hotmail.com>
2023-02-18 21:16:41 +0100

    Added build skript

---
+++ build.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+#
+
+# This file is 
+USAGE="Usage: build.sh [OPTIONS]
+Builds an image.
+
+Usage:
+ build.sh bitbake <same arguments as bitbake>
+ build.sh runqemu qemux86
+"
+
+if [ -z "$*" ]; then
+    usage
+fi
+
+OE_INIT="../sources/poky/"
+. ${OE_INIT}/oe-init-build-env . >/dev/null 2>&1
+
+$@ || \
+    FAILED "$*"
+
